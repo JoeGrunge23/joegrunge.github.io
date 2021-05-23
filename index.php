34fcfa93932623b74fe9e39751847d8801fdfbea
JoeGrunge23 <joerizcasiple@hotmail.com>
2021-05-24 06:07:43 +0800

    updated

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="css/style.css">
+    <title>Joeriz Casiple</title>
+</head>
+<body>
+<nav id="nav">
+    <ul>
+      <li><a href="#nav">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+     </ul>
+  </nav>
+
+  <header id="header">
+    <div>
+      <h1>HI! <span>I'm <span>Joeriz </span>!</span></h1>
+      <h2>I'm a <span> Programmer </span></h2>
+      
+      <a href="#about">
+        Let's Get Started! 
+      </a>
+    </div>
+  </header>
+
+  <article id="about">
+    <div>
+      <h1>A<span>bout</span></h1>
+      <h2>I LOVE CODING! </h2>
+    </div>
+
+    <div class='profile'>
+      <div>
+        <h1>Profile</h1>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+           sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+      </div>
+      <div>
+        <img src="imgs/kurt.jpg" alt="">
+      </div>
+      <div>
+        <h1>Details</h1>
+        <h2>Name: </h2>
+        <p>Joeriz Casiple</p>
+        <h2>Age: </h2>
+        <p>25</p>
+        <h2>Location: </h2>
+        <p>Davao City, Philippines</p>
+      </div>
+      <div>
+        <p> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in </p>
+      </div>
+    </div>
+
+  </article>
+  
+  <article id="portfolio">portfolio</article>
+  <!-- <article id="blog">blog</article> -->
+  <article id="contact">contact</article>
+<footer id="footer">footer</footer>
+</body>
+</html>
